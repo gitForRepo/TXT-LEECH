ed3ba66514500ba8543ca4cdd73229a697d67478
Venom <amanblazemusicrecords@gmail.com>
2025-02-10 15:45:48 +0530

    Update vars.py

--- plugins/vars.py
+++ plugins/vars.py
@@ -1,8 +1,8 @@
 import os
 
-API_ID    = os.environ.get("API_ID", "")
-API_HASH  = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+API_ID    = os.environ.get("API_ID", 12283173515)
+API_HASH  = os.environ.get("API_HASH", "be786a8240abf6e12283173515e2f833")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8113578720:AAGuDsG3Gqa6XkwurIFbQRX5y3LwrtbTeU8") 
 
 WEBHOOK = True  # Don't change this
 PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
